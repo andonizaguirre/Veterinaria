26ffe7b843cb9e4df5d0e388b19bf86c0f1d3342
andonizaguirre <48142479+andonizaguirre@users.noreply.github.com>
2023-02-23 00:08:43 -0500

    Add files via upload

---
+++ 20221_frontend_veterinaria-master/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import CRUDMascotasPage from './pages/CRUDMascotas.page';
+
+function App() {
+    return (
+        <div className="App">
+            <CRUDMascotasPage />
+        </div>
+    );
+}
+
+export default App;
